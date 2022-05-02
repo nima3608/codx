71da735b04e60ebef9567d014e27be030d9b9cdc
Nima Fakhari <nima.fakhari@gmail.com>
2022-05-02 12:03:52 +0430

    e

---
+++ CodeCoverage/Controllers/ValuesController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CodeCoverage.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeCoverage.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private IOrderService _orderService;
+
+        public ValuesController(IOrderService orderService)
+        {
+            _orderService = orderService ?? throw new ArgumentNullException(nameof(orderService));
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ CodeCoverageTests/Controllers/ValuesControllerTests.cs
@@ -0,0 +1,21 @@
+﻿using CodeCoverage.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeCoverageTests.Controllers
+{
+    [TestClass]
+    public class ValuesControllerTests
+    {
+        [TestMethod]
+        public void Throws_ANE_OrderService()
+        {
+            // Arrange
+            // Act 
+            // Assert
+            Assert.ThrowsException<ArgumentNullException>(() => new ValuesController(orderService: null));
+        }
+    }
+}
